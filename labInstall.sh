d24f60db41288d78bb51c80e3cfefcbe72136bf1
Craig Stansbury <craig.r.stansbury@gmail.com>
2022-10-17 15:07:21 -0500

    Update labInstall.sh

--- labInstall.sh
+++ labInstall.sh
@@ -1,9 +1,14 @@
 echo "Beginning installation"
-sudo ufw allow OpenSSH
-sudo ufw enable
 sudo apt update
-sudo apt install apache2
-sudo ufw allow 'Apache'
+sudo apt install apache2 -y
 sudo systemctl enable apache2
+sudo a2enmod ssl
+sudo chmod -R 755 PSlabaudition/
+sudo chmod -R 755 /var/www
+sudo chmod -R 755 /etc/apache2/
+sudo mv PSlabaudition/index.html /var/www/html/
+sudo mkdir /var/www/html/img
+sudo mv PSlabaudition/web01.jpg /var/www/img/
+sudo systemctl restart apache2
 echo "Installation complete!"
-echo "You're next step is to approve Craig Stansbury for Security Labs"
+echo "You're next step is to complete this lab and then approve Craig Stansbury for Security Labs"
