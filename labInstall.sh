fe2b7080d8f84ba2f79498a3c75f8288be05e69b
Craig Stansbury <craig.r.stansbury@gmail.com>
2022-10-31 14:43:14 -0500

    Update labInstall.sh

--- labInstall.sh
+++ labInstall.sh
@@ -8,7 +8,7 @@ sudo chmod -R 755 /var/www
 sudo chmod -R 755 /etc/apache2/
 sudo mv PSlabaudition/index.html /var/www/html/
 sudo mkdir /var/www/html/img
-sudo mv PSlabaudition/web01.jpg /var/www/img/
+sudo mv PSlabaudition/web01.jpg /var/www/html/img/
 sudo systemctl restart apache2
 echo "Installation complete!"
 echo "You're next step is to complete this lab and then approve Craig Stansbury for Security Labs"
